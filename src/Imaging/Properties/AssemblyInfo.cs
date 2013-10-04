ad1d54ef5bff11e6144ae8848a7798eb1966c8bd
Thomas Sherwood <t.sherwood@me.com>
2013-10-04 23:34:18 +0100

    Altered builds paths to use environment variable

--- src/Imaging/Properties/AssemblyInfo.cs
+++ src/Imaging/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "Femore.Imaging" )]
+[assembly: AssemblyTitle( "Femore.Imaging.Core" )]
 [assembly: AssemblyDescription( "Image Processing module" )]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
