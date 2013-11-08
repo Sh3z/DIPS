02026074c5a26363c76e84dfea72467d8c3c1f9a
Gary_Chadwick <Gary.Chadwick1@btinternet.com>
2013-11-08 11:13:43 +0000

    Addition of several dialogs for prototype demo and discussion of GUI with group

---
+++ FemoreGUI/Controls/ImageInfo/ImageInfo.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FemoreGUI.Controls
+{
+    /// <summary>
+    /// Interaction logic for ImageInfo.xaml
+    /// </summary>
+    public partial class ImageInfo : UserControl
+    {
+        public ImageInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
