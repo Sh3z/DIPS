8b3846c2ed448929ee5e661c27125c5d6ba88a4c
GaryClaret <Gary.Chadwick1@btinternet.com>
2013-11-22 17:24:40 +0000

    Added loadDS3, which I must have forgotten to commit last time.

---
+++ src/UI/Pages/LoadNewDataset/LoadNewDSStep3.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DIPS.UI.Pages;
+
+namespace DIPS.UI.Pages.LoadNewDataset
+{
+    /// <summary>
+    /// Interaction logic for LoadNewDSStep3.xaml
+    /// </summary>
+    public partial class LoadNewDSStep3 : Page
+    {
+        public LoadNewDSStep3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Images now being processed");
+
+            MainPage main = new MainPage();
+            this.NavigationService.Navigate(main);
+        }
+    }
+}
