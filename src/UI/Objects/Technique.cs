9a614754ba929425e944972d7d45acdce3f25ab6
GaryClaret <Gary.Chadwick1@btinternet.com>
2014-01-17 12:29:31 +0000

    Checking in additions to setp 1 - partway through step 2.

---
+++ src/UI/Objects/Technique.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPS.UI.Objects
+{
+    public class Technique
+    {
+        private int _ID;
+
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+        private String _Name;
+
+        public String Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+        
+    }
+}
