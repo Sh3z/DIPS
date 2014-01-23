4351af9c133d9754321aabff12651ec7b7aaf589
Sh3z <t.sherwood@me.com>
2014-01-23 17:19:43 +0000

    AlgorithmPlugin

--- src/Processor/Plugin/AlgorithmPlugin.cs
+++ src/Processor/Plugin/AlgorithmPlugin.cs
@@ -7,20 +7,43 @@ using System.Threading.Tasks;
 
 namespace DIPS.Processor.Plugin
 {
+    /// <summary>
+    /// Represents the base class of all plugins within the algorithm system. This class
+    /// is abstract.
+    /// </summary>
     public abstract class AlgorithmPlugin
     {
+        /// <summary>
+        /// Gets or sets the input <see cref="Bitmap"/> to this <see cref="AlgorithmPlugin"/>.
+        /// </summary>
         public Bitmap Input
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gets the output of this <see cref="AlgorithmPlugin"/> generated after executing
+        /// the Run method on this <see cref="AlgorithmPlugin"/>.
+        /// </summary>
         public Bitmap Output
         {
             get;
             protected set;
         }
 
+        /// <summary>
+        /// Executes the algorithm using the provided input.
+        /// </summary>
+        /// <param name="args">The set of input arguments when executing this plugin.</param>
         public abstract void Run( RunArgs args );
+
+        /// <summary>
+        /// Manufactures a collection of <see cref="Property"/> objects representing the
+        /// parameters that can be modified within this <see cref="AlgorithmPlugin"/>.
+        /// </summary>
+        /// <returns>A collection of <see cref="Property"/> objects representing the parameters
+        /// that can be modified.</returns>
+        public abstract ICollection<Property> CreatePropertyDefinitions();
     }
 }
