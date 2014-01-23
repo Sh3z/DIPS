d8f5295e25e7b0dd07939c7d3600f52c9436939c
Sh3z <t.sherwood@me.com>
2014-01-23 17:08:34 +0000

    Added plugin tests

--- src/Processor/Plugin/Property.cs
+++ src/Processor/Plugin/Property.cs
@@ -30,6 +30,9 @@ namespace DIPS.Processor.Plugin
             {
                 throw new ArgumentNullException( "type" );
             }
+
+            Name = name;
+            Type = type;
         }
 
         /// <summary>
@@ -64,12 +67,12 @@ namespace DIPS.Processor.Plugin
             }
             set
             {
-                if( _value == null )
+                if( value == null )
                 {
                     return;
                 }
 
-                if( _value.GetType() != Type )
+                if( _isValidType( value.GetType() ) == false )
                 {
                     throw new ArgumentException( "Invalid Value type." );
                 }
@@ -79,5 +82,31 @@ namespace DIPS.Processor.Plugin
         }
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private object _value;
+
+
+        /// <summary>
+        /// Checks if the incoming type is compatible with that of this property's.
+        /// </summary>
+        /// <param name="t">The incoming type.</param>
+        /// <returns>true if the incoming type is acceptable.</returns>
+        private bool _isValidType( Type t )
+        {
+            if( t == Type )
+            {
+                return true;
+            }
+
+            if( t.IsSubclassOf( Type ) )
+            {
+                return true;
+            }
+
+            if( Type.IsInterface && t.GetInterfaces().Contains( Type ) )
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
