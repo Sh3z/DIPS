6b51c066386e449110a3bfb26316f4130c216251
Sh3z <t.sherwood@me.com>
2014-01-31 17:25:10 +0000

    Initial compressor framework components

---
+++ src/Processor/Client/XML/CompressorAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPS.Processor.XML
+{
+    /// <summary>
+    /// Exposes <see cref="ICompressor"/> classes to the factory. This class
+    /// cannot be inherited.
+    /// </summary>
+    public sealed class CompressorAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompressorAttribute"/>
+        /// class.
+        /// </summary>
+        /// <param name="identifier">The unique identifier of the compressor.</param>
+        /// <exception cref="ArgumentException">identifier is null or
+        /// empty.</exception>
+        public CompressorAttribute( string identifier )
+        {
+            if( string.IsNullOrEmpty( identifier ) )
+            {
+                throw new ArgumentException( "identifier" );
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique identifier for the compressor.
+        /// </summary>
+        public string Identifier
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ src/Processor/Client/XML/ICompressor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPS.Processor.XML
+{
+    /// <summary>
+    /// Represents a raw-byte compressor used for compressing and decompressing
+    /// <see cref="byte"/> arrays.
+    /// </summary>
+    public interface ICompressor
+    {
+        /// <summary>
+        /// Compresses an array of <see cref="byte"/>s into a new array.
+        /// </summary>
+        /// <param name="toCompress">The <see cref="byte"/> array to
+        /// compress.</param>
+        /// <returns>An array of <see cref="byte"/>s representing the
+        /// compressed form of the input.</returns>
+        byte[] Compress( byte[] toCompress );
+
+        /// <summary>
+        /// Decompresses an array of <see cref="byte"/>s back into their
+        /// original state.
+        /// </summary>
+        /// <param name="toDecompress">The array of <see cref="byte"/>s to
+        /// decompress.</param>
+        /// <returns>An array of <see cref="byte"/>s representing their original
+        /// state.</returns>
+        byte[] Decompress( byte[] toDecompress );
+    }
+}
