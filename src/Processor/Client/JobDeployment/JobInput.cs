789ba4db15aa2a2cd5b612d0e0e2b8a70ccfe15c
Sh3z <t.sherwood@me.com>
2014-02-01 17:43:13 +0000

    Added job definition stubs

---
+++ src/Processor/Client/JobDeployment/JobInput.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPS.Processor.Client.JobDeployment
+{
+    public sealed class JobInput
+    {
+        public JobInput( Image input )
+        {
+            if( input == null )
+            {
+                throw new ArgumentNullException( "input" );
+            }
+
+            Input = input;
+        }
+
+
+        public Image Input
+        {
+            get;
+            private set;
+        }
+
+        public object Identifier
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ src/Processor/Client/JobDeployment/ObjectJobDefinition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPS.Processor.Client.JobDeployment
+{
+    public class ObjectJobDefinition : IJobDefinition
+    {
+        public ObjectJobDefinition( IEnumerable<AlgorithmDefinition> algorithms, IEnumerable<JobInput> inputs )
+        {
+            if( algorithms == null )
+            {
+                throw new ArgumentNullException( "algorithms" );
+            }
+
+            if( inputs == null )
+            {
+                throw new ArgumentNullException( "inputs" );
+            }
+        }
+
+        public IEnumerable<AlgorithmDefinition> GetAlgorithms()
+        {
+            return _algorithms;
+        }
+
+        public IEnumerable<JobInput> GetInputs()
+        {
+            return _inputs;
+        }
+
+
+        private IEnumerable<AlgorithmDefinition> _algorithms;
+
+        private IEnumerable<JobInput> _inputs;
+    }
+}
