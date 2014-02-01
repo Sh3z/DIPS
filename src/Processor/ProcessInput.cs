110b83401323b17e25423a5866c4fdecdc60a165
Sh3z <t.sherwood@me.com>
2014-02-01 15:35:40 +0000

    Added input and job stubs

---
+++ src/Processor/ProcessInput.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPS.Processor
+{
+    public class ProcessInput
+    {
+        public ProcessInput( Image input )
+        {
+            if( input == null )
+            {
+                throw new ArgumentNullException( "input" );
+            }
+
+            Image = input;
+        }
+
+        public Image Image
+        {
+            get;
+            private set;
+        }
+
+        public object Identifier
+        {
+            get;
+            set;
+        }
+
+        public bool HasIdentifier
+        {
+            get
+            {
+                return Identifier != null;
+            }
+        }
+    }
+}
