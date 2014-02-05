5ed9f031811dd9f35e21c94f9f1a027a7ca9dabb
Sh3z <t.sherwood@me.com>
2014-02-05 16:44:58 +0000

    Added comments

--- src/Processor/Client/IJobTicket.cs
+++ src/Processor/Client/IJobTicket.cs
@@ -2,29 +2,55 @@
 
 namespace DIPS.Processor.Client
 {
+    /// <summary>
+    /// Represents an entry into the queueing system for a single job.
+    /// </summary>
     public interface IJobTicket
     {
+        /// <summary>
+        /// Occurs when the job is cancelled.
+        /// </summary>
         event EventHandler JobCancelled;
 
+        /// <summary>
+        /// Occurs when the job is complete.
+        /// </summary>
         event EventHandler JobCompleted;
 
+        /// <summary>
+        /// Occurs when the job has begun.
+        /// </summary>
         event EventHandler JobStarted;
 
+        /// <summary>
+        /// Gets the original <see cref="JobRequest"/> provided by the client.
+        /// </summary>
         JobRequest Request
         {
             get;
         }
 
+        /// <summary>
+        /// Gets the constructed <see cref="JobResult"/> for the job.
+        /// </summary>
         JobResult Result
         {
             get;
         }
 
+        /// <summary>
+        /// Gets a value indicating whether this job was cancelled by the client.
+        /// </summary>
         bool Cancelled
         {
             get;
         }
 
+        /// <summary>
+        /// Cancels this job. If it is running, it is stopped during execution. If
+        /// it has not run, it is removed from the queue. If the job has already finished,
+        /// nothing occurs.
+        /// </summary>
         void Cancel();
     }
 }
--- src/Processor/JobTicket.cs
+++ src/Processor/JobTicket.cs
@@ -7,8 +7,17 @@ using System.Threading.Tasks;
 
 namespace DIPS.Processor
 {
+    /// <summary>
+    /// Represents an entry into the queueing system for a single job.
+    /// </summary>
     public class JobTicket : IJobTicket
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JobTicket"/> class.
+        /// </summary>
+        /// <param name="req">The <see cref="JobRequest"/> this ticket
+        /// represents within the queue.</param>
+        /// <exception cref="ArgumentNullException">req is null.</exception>
         public JobTicket( JobRequest req )
         {
             if( req == null )
@@ -31,31 +40,54 @@ namespace DIPS.Processor
             private set;
         }
 
+        /// <summary>
+        /// Occurs when the job is cancelled.
+        /// </summary>
         public event EventHandler JobCancelled;
 
+        /// <summary>
+        /// Occurs when the job has begun.
+        /// </summary>
         public event EventHandler JobStarted;
 
+        /// <summary>
+        /// Occurs when the job is complete.
+        /// </summary>
         public event EventHandler JobCompleted;
 
 
+        /// <summary>
+        /// Gets a value indicating whether this job was cancelled by the client.
+        /// </summary>
         public bool Cancelled
         {
             get;
             private set;
         }
 
+        /// <summary>
+        /// Gets the original <see cref="JobRequest"/> provided by the client.
+        /// </summary>
         public JobRequest Request
         {
             get;
             private set;
         }
 
+        /// <summary>
+        /// Gets the constructed <see cref="JobResult"/> for the job.
+        /// </summary>
         public JobResult Result
         {
             get;
             internal set;
         }
 
+        /// <summary>
+        /// Cancels this job. If it is running, it is stopped during execution. If
+        /// it has not run, it is removed from the queue. If the job has already finished,
+        /// nothing occurs.
+        /// </summary>
         public void Cancel()
         {
             Cancelled = true;
