a50713b3faff6767a47b78284104f2d4b4cd298b
Sh3z <t.sherwood@me.com>
2014-02-05 17:06:06 +0000

    Added abstract ticket handler

---
+++ src/Processor/ITicketCancellationHandler.cs
@@ -0,0 +1,37 @@
+﻿using DIPS.Processor.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPS.Processor
+{
+    /// <summary>
+    /// Represents an object capable of handling the request to cancel an
+    /// <see cref="IJobTicket"/>.
+    /// </summary>
+    public interface ITicketCancellationHandler
+    {
+        /// <summary>
+        /// Sets the successive <see cref="ITicketCancellationHandler"/> to
+        /// this instance.
+        /// </summary>
+        /// <remarks>
+        /// In the event the current <see cref="ITicketCancellationHandler"/>
+        /// cannot handle the cancellation of a ticket, it should instead delegate
+        /// to the successor. If no successor is set, the request should be ignored.
+        /// </remarks>
+        ITicketCancellationHandler Successor
+        {
+            set;
+        }
+
+        /// <summary>
+        /// Handles the cancellation of the job represented by the ticket.
+        /// </summary>
+        /// <param name="ticket">The <see cref="IJobTicket"/> that has been
+        /// cancelled.</param>
+        void Handle( IJobTicket ticket );
+    }
+}
