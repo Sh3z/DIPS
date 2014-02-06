84b39ebf3a0594ad396f3af5e24bdec85fa07fb2
Sh3z <t.sherwood@me.com>
2014-02-06 23:09:35 +0000

    Added more information to job result

--- src/Processor/Client/JobResult.cs
+++ src/Processor/Client/JobResult.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,37 @@ namespace DIPS.Processor.Client
     public class JobResult
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="JobResult"/> class.
+        /// Allows private construction of custom job states.
+        /// </summary>
+        /// <param name="state">The state to expost.</param>
+        private JobResult( JobState state )
+        {
+            Result = state;
+            Images = new ProcessedImageSet();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JobResult"/> class,
+        /// representing a job that encountered an error.
+        /// </summary>
+        /// <param name="err">The <see cref="Exception"/> that caused the
+        /// job to terminate.</param>
+        /// <exception cref="ArgumentNullException">err is null.</exception>
+        public JobResult( Exception err )
+        {
+            if( err == null )
+            {
+                throw new ArgumentNullException( "err" );
+            }
+
+            Exception = err;
+            Result = JobState.Error;
+            Images = new ProcessedImageSet();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JobResult"/> class,
+        /// representing a complete job.
         /// </summary>
         /// <param name="images">The set of processed images returned by
         /// the service.</param>
@@ -25,6 +56,41 @@ namespace DIPS.Processor.Client
             }
 
             Images = new ProcessedImageSet( images );
+            Result = JobState.Complete;
+        }
+
+
+        /// <summary>
+        /// Represents the <see cref="JobResult"/> used to express a cancelled
+        /// job.
+        /// </summary>
+        public static JobResult Cancelled
+        {
+            get
+            {
+                return _cancelled;
+            }
+        }
+        [DebuggerBrowsable( DebuggerBrowsableState.Never )]
+        private static JobResult _cancelled = new JobResult( JobState.Cancelled );
+
+        /// <summary>
+        /// Provides an indication as to how the job finished.
+        /// </summary>
+        public JobState Result
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Exception"/> that caused the job to stop. If the
+        /// job completed successfully or was cancelled, this is null.
+        /// </summary>
+        public Exception Exception
+        {
+            get;
+            private set;
         }
 
         /// <summary>
---
+++ src/Processor/Client/JobState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPS.Processor.Client
+{
+    /// <summary>
+    /// Represents the completion states of a job.
+    /// </summary>
+    public enum JobState
+    {
+        /// <summary>
+        /// The job was cancelled before it could complete.
+        /// </summary>
+        Cancelled,
+
+        /// <summary>
+        /// The job encountered a critical error during execution and
+        /// could not continue.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// The job completed successfully.
+        /// </summary>
+        Complete
+    }
+}
