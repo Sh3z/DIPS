61356e6c8f3b52251740415ff53f69f534270f34
Sh3z <t.sherwood@me.com>
2014-02-06 22:54:52 +0000

    Exposed processed images in JobResult

--- src/Processor/Client/JobResult.cs
+++ src/Processor/Client/JobResult.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace DIPS.Processor.Client
 {
+    /// <summary>
+    /// Represents the information returned by the service upon a complete job.
+    /// </summary>
     public class JobResult
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JobResult"/> class.
+        /// </summary>
+        /// <param name="images">The set of processed images returned by
+        /// the service.</param>
+        /// <exception cref="ArgumentNullException">images is null.</exception>
+        public JobResult( IEnumerable<IProcessedImage> images )
+        {
+            if( images == null )
+            {
+                throw new ArgumentNullException( "images" );
+            }
+
+            Images = new ProcessedImageSet( images );
+        }
+
+        /// <summary>
+        /// Gets the <see cref="ProcessedImageSet"/> containing the results of
+        /// each processed image.
+        /// </summary>
+        public ProcessedImageSet Images
+        {
+            get;
+            private set;
+        }
     }
 }
