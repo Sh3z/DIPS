6d30ea11891607e269451c86c56b4f5427b37d86
Sh3z <t.sherwood@me.com>
2014-02-06 12:48:14 +0000

    Refactored plugin creation to factory

---
+++ Tests/Processor/Processor/RegistryFactoryTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DIPS.Processor;
+using DIPS.Processor.Plugin;
+using DIPS.Processor.Client;
+
+namespace DIPS.Tests.Processor
+{
+    /// <summary>
+    /// Summary description for RegistryFactoryTests
+    /// </summary>
+    [TestClass]
+    public class RegistryFactoryTests
+    {
+        /// <summary>
+        /// Gets or sets the test context which provides
+        /// information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Tests manufacturing a plugin using an invalid definition.
+        /// </summary>
+        [TestMethod]
+        public void TestManufacture_InvalidDefintion()
+        {
+            RegistryFactory factory = new RegistryFactory();
+            AlgorithmPlugin p = factory.Manufacture( null );
+            Assert.IsNull( p );
+        }
+
+        /// <summary>
+        /// Tests manufacturing a plugin using a valid definition.
+        /// </summary>
+        [TestMethod]
+        public void TestManufacture_ValidDefinition()
+        {
+            AlgorithmDefinition d = new AlgorithmDefinition( "gamma", new Property[] {} );
+            RegistryFactory factory = new RegistryFactory();
+            AlgorithmPlugin p = factory.Manufacture( d );
+            Assert.IsNotNull( p );
+        }
+    }
+}
---
+++ src/Processor/RegistryFactory.cs
@@ -0,0 +1,55 @@
+﻿using DIPS.Processor.Client;
+using DIPS.Processor.Plugin;
+using DIPS.Processor.Registry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPS.Processor
+{
+    /// <summary>
+    /// Represents the plugin factory used to manufacture plugins using the
+    /// types loaded from the registry.
+    /// </summary>
+    public class RegistryFactory : IPluginFactory
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegistryFactory"/>
+        /// class.
+        /// </summary>
+        public RegistryFactory()
+        {
+            _activator = new AlgorithmActivator( RegistryCache.Cache );
+        }
+
+
+        /// <summary>
+        /// Manufactures an appropriate <see cref="AlgorithmPlugin"/> given the
+        /// provided <see cref="AlgorithmDefinition"/>.
+        /// </summary>
+        /// <param name="algorithm">The <see cref="AlgorithmDefinition"/>
+        /// descriving the algorithm to create.</param>
+        /// <returns>An instance of <see cref="AlgorithmPlugin"/> represented
+        /// by the given definition, or null if an algorithm cannot be built
+        /// with the provided definition.</returns>
+        public AlgorithmPlugin Manufacture( AlgorithmDefinition algorithm )
+        {
+            try
+            {
+                return _activator.Activate( algorithm );
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// Contains the private activator.
+        /// </summary>
+        private AlgorithmActivator _activator;
+    }
+}
