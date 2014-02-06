980d2bc857f08eda4f4f521782c384b996d4b6ff
Sh3z <t.sherwood@me.com>
2014-02-06 01:32:00 +0000

    Added IPropertyInterpreter

---
+++ src/Processor/Plugin/IPropertyInterpreter.cs
@@ -0,0 +1,23 @@
+﻿using DIPS.Processor.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPS.Processor.Plugin
+{
+    /// <summary>
+    /// Represents an object that accepts a set of <see cref="Property"/>
+    /// objects and interprets them into a new purpose.
+    /// </summary>
+    public interface IPropertyInterpreter
+    {
+        /// <summary>
+        /// Interprets the incoming set of <see cref="Property"/> objects.
+        /// </summary>
+        /// <param name="properties">The <see cref="PropertySet"/> containing
+        /// an aggregation of property values.</param>
+        void Interpret( PropertySet properties );
+    }
+}
