fca86bb51cac6f33c5931a9fed97ac9c2d8245fb
Sh3z <t.sherwood@me.com>
2014-02-06 16:04:52 +0000

    Added AppDataPersister

---
+++ Tests/Processor/Processor/AppDataPersisterTests.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DIPS.Processor.Persistence;
+using DIPS.Processor;
+using DIPS.Processor.Client;
+using DIPS.Processor.Client.JobDeployment;
+using System.Drawing;
+using DIPS.Processor.Plugin;
+using System.IO;
+using System.Linq;
+
+namespace DIPS.Tests.Processor
+{
+    /// <summary>
+    /// Summary description for AppDataPersisterTests
+    /// </summary>
+    [TestClass]
+    public class AppDataPersisterTests
+    {
+        /// <summary>
+        /// Gets or sets the test context which provides
+        /// information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Gets or sets the current ticket to use in tests.
+        /// </summary>
+        private JobTicket CurrentTicket
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Initializer called before tests.
+        /// </summary>
+        [TestInitialize]
+        public void Initialize()
+        {
+            JobRequest r = new JobRequest( new TestDefinition() );
+            CurrentTicket = new JobTicket( r, new TestCancellation() );
+        }
+
+        /// <summary>
+        /// Cleaner called between tests.
+        /// </summary>
+        [TestCleanup]
+        public void Clean()
+        {
+            // Delete all files in our test directory.
+            if( Directory.Exists( AppDataPersister.OutputDataPath ) )
+            {
+                Directory.Delete( AppDataPersister.OutputDataPath, true );
+            }
+        }
+
+
+        /// <summary>
+        /// Tests constructing the persister with a null ticket
+        /// </summary>
+        [TestMethod]
+        [ExpectedException( typeof( ArgumentNullException ) )]
+        public void TestConstructor_NullTicket()
+        {
+            AppDataPersister persister = new AppDataPersister( null );
+        }
+
+        /// <summary>
+        /// Tests persisting an image with no identifier.
+        /// </summary>
+        [TestMethod]
+        public void TestPersist_NoIdentifier()
+        {
+            AppDataPersister persister = new AppDataPersister( CurrentTicket );
+            Image toPersist = CurrentTicket.Request.Job.GetInputs().First().Input;
+            object identifier = null;
+            persister.Persist( toPersist, identifier );
+
+            // File should be called output_0.png
+            string path = string.Format( @"{0}/{{{1}}}/output_0.png", AppDataPersister.OutputDataPath, CurrentTicket.JobID );
+            bool fileExists = File.Exists( path );
+            Assert.IsTrue( fileExists );
+
+            // Persist again, should be output_1.png
+            persister.Persist( toPersist, identifier );
+
+            path = string.Format( @"{0}/{{{1}}}/output_1.png", AppDataPersister.OutputDataPath, CurrentTicket.JobID );
+            fileExists = File.Exists( path );
+            Assert.IsTrue( fileExists );
+        }
+
+        /// <summary>
+        /// Tests persisting an image with an identifier.
+        /// </summary>
+        [TestMethod]
+        public void TestPersist_WithIdentifier()
+        {
+            string id = "test";
+            AppDataPersister persister = new AppDataPersister( CurrentTicket );
+            Image toPersist = CurrentTicket.Request.Job.GetInputs().First().Input;
+            persister.Persist( toPersist, id );
+
+            // File should be called output_0.png
+            string path = string.Format( @"{0}/{{{1}}}/{2}.png", AppDataPersister.OutputDataPath, CurrentTicket.JobID, id );
+            bool fileExists = File.Exists( path );
+            Assert.IsTrue( fileExists );
+        }
+
+
+        class TestDefinition : IJobDefinition
+        {
+            public IEnumerable<AlgorithmDefinition> GetAlgorithms()
+            {
+                return new AlgorithmDefinition[] {
+                    new AlgorithmDefinition("Test", null ) };
+            }
+
+            public IEnumerable<JobInput> GetInputs()
+            {
+                return new[] { new JobInput( Image.FromFile( "img.bmp" ) ) };
+            }
+        }
+
+        class TestAlgorithm : AlgorithmPlugin
+        {
+            public override void Run()
+            {
+                Output = Input;
+            }
+        }
+
+        class TestCancellation : ITicketCancellationHandler
+        {
+            public ITicketCancellationHandler Successor
+            {
+                get;
+                set;
+            }
+
+            public bool Handle( IJobTicket ticket )
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ src/Processor/Persistence/AppDataPersister.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPS.Processor.Persistence
+{
+    /// <summary>
+    /// Represents the job persister used to save jobs to the Windows
+    /// APPDATA directory.
+    /// </summary>
+    public class AppDataPersister : IJobPersister
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppDataPersister"/>
+        /// class.
+        /// </summary>
+        /// <param name="ticket">The <see cref="JobTicket"/> this persister
+        /// will save jobs for.</param>
+        /// <exception cref="ArgumentNullException">ticket is null.</exception>
+        public AppDataPersister( JobTicket ticket )
+        {
+            if( ticket == null )
+            {
+                throw new ArgumentNullException( "ticket" );
+            }
+
+            _ticket = ticket;
+        }
+
+
+        /// <summary>
+        /// Gets the root directory of the DIPS AppData path.
+        /// </summary>
+        public static string RootDataPath
+        {
+            get
+            {
+                return string.Format( @"{0}/DIPS", Environment.GetFolderPath( Environment.SpecialFolder.ApplicationData ) );
+            }
+        }
+
+        /// <summary>
+        /// Gets the output directory of the DIPS AppData path.
+        /// </summary>
+        public static string OutputDataPath
+        {
+            get
+            {
+                return string.Format( @"{0}/Output", RootDataPath );
+            }
+        }
+
+
+        /// <summary>
+        /// Persits the output of a job.
+        /// </summary>
+        /// <param name="output">The <see cref="Image"/> generated from a
+        /// complete job.</param>
+        /// <param name="identifier">The identifier for the input provided
+        /// by the client.</param>
+        public void Persist( Image output, object identifier )
+        {
+            string appDataPath = Environment.GetFolderPath( Environment.SpecialFolder.ApplicationData );
+            if( Directory.Exists( appDataPath ) == false )
+            {
+                throw new IOException( "AppData cannot be resolved." );
+            }
+
+            Guid jobGuid = _ticket.JobID;
+            string fullPath = _resolvePath( identifier, jobGuid );
+            using( Stream stream = File.Create( fullPath ) )
+            {
+                output.Save( stream, ImageFormat.Png );
+            }
+        }
+
+
+        /// <summary>
+        /// Resolves the full path to save the image to.
+        /// </summary>
+        /// <param name="identifier">The identifier provided</param>
+        /// <param name="jobGuid">The Guid of the ticket</param>
+        /// <returns>The path to the file to save the image to.</returns>
+        private string _resolvePath( object identifier, Guid jobGuid )
+        {
+            string jobPath = string.Format( @"{0}/{{{1}}}", OutputDataPath, jobGuid );
+            if( Directory.Exists( jobPath ) == false )
+            {
+                Directory.CreateDirectory( jobPath );
+            }
+
+            string fileName = identifier != null ? identifier.ToString() : null;
+            if( string.IsNullOrEmpty( fileName ) )
+            {
+                fileName = string.Format( @"output_{0}", _id );
+                _id++;
+            }
+
+            return string.Format( @"{0}/{1}.png", jobPath, fileName );
+        }
+
+
+        /// <summary>
+        /// Retains the ticket of the job.
+        /// </summary>
+        private JobTicket _ticket;
+
+        /// <summary>
+        /// Retains the current id for jobs with no identifiers.
+        /// </summary>
+        private int _id;
+    }
+}
