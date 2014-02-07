4ec079852aae8f2d8746f2d99561bc8acbc50791
Sh3z <t.sherwood@me.com>
2014-02-07 16:25:43 +0000

    Added definition xml validator

---
+++ Tests/Processor/XML/DefinitionXmlValidatorTests.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Linq;
+using DIPS.Processor.XML.Decompilation;
+
+namespace DIPS.Tests.Processor.XML
+{
+    /// <summary>
+    /// Summary description for DefinitionXmlValidatorTests
+    /// </summary>
+    [TestClass]
+    public class DefinitionXmlValidatorTests
+    {
+        /// <summary>
+        /// Gets or sets the test context which provides
+        /// information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Tests validating a node that is not an XElement
+        /// </summary>
+        [TestMethod]
+        public void TestValidateAlgorithm_NotAnElement()
+        {
+            XText text = new XText( "test" );
+            DudVisitor visitor = new DudVisitor();
+            DefinitionXmlValidator validator = new DefinitionXmlValidator( visitor );
+            validator.VisitAlgorithm( text );
+
+            Assert.IsFalse( visitor.DidVisitAlgorithm );
+        }
+
+        /// <summary>
+        /// Tests validating an XElement with the wrong name
+        /// </summary>
+        [TestMethod]
+        public void TestValidateAlgorithm_InvalidElementName()
+        {
+            XElement element = new XElement( "BadElement" );
+            DudVisitor visitor = new DudVisitor();
+            DefinitionXmlValidator validator = new DefinitionXmlValidator( visitor );
+            validator.VisitAlgorithm( element );
+
+            Assert.IsFalse( visitor.DidVisitAlgorithm );
+        }
+
+        /// <summary>
+        /// Tests validating an algorithm without the name attribute
+        /// </summary>
+        [TestMethod]
+        public void TestValidateAlgorithm_NoAlgorithmNameAttribute()
+        {
+            XElement element = new XElement( "algorithm" );
+            DudVisitor visitor = new DudVisitor();
+            DefinitionXmlValidator validator = new DefinitionXmlValidator( visitor );
+            validator.VisitAlgorithm( element );
+
+            Assert.IsFalse( visitor.DidVisitAlgorithm );
+        }
+
+        /// <summary>
+        /// Tests validating an algorithm with the name attribute, but with
+        /// no value
+        /// </summary>
+        [TestMethod]
+        public void TestValidateAlgorithm_NameAttributeNoValue()
+        {
+            XElement element = new XElement( "algorithm", new XAttribute( "name", string.Empty ) );
+            DudVisitor visitor = new DudVisitor();
+            DefinitionXmlValidator validator = new DefinitionXmlValidator( visitor );
+            validator.VisitAlgorithm( element );
+
+            Assert.IsFalse( visitor.DidVisitAlgorithm );
+        }
+
+        /// <summary>
+        /// Tests validating an algorithm with no properties.
+        /// </summary>
+        [TestMethod]
+        public void TestValidateAlgorithm_NoProperties()
+        {
+            XElement element = new XElement( "algorithm", new XAttribute( "name", "test" ) );
+            DudVisitor visitor = new DudVisitor();
+            DefinitionXmlValidator validator = new DefinitionXmlValidator( visitor );
+            validator.VisitAlgorithm( element );
+
+            Assert.IsTrue( visitor.DidVisitAlgorithm );
+        }
+
+        /// <summary>
+        /// Tests validating an algorithm with a property missing the name attribute.
+        /// </summary>
+        [TestMethod]
+        public void TestValidateAlgorithm_OneProperty_NoNameAttribute()
+        {
+            XElement element = new XElement(
+                "algorithm", new XAttribute( "name", "test" ),
+                new XElement( "properties", new XElement( "property" ) ) );
+            DudVisitor visitor = new DudVisitor();
+            DefinitionXmlValidator validator = new DefinitionXmlValidator( visitor );
+            validator.VisitAlgorithm( element );
+
+            Assert.IsFalse( visitor.DidVisitAlgorithm );
+        }
+
+        /// <summary>
+        /// Tests validating a property with a name attribute that has no value
+        /// </summary>
+        [TestMethod]
+        public void TestValidateAlgorithm_OneProperty_NoNameValue()
+        {
+            XElement element = new XElement(
+                "algorithm", new XAttribute( "name", "test" ),
+                new XElement( "properties",
+                    new XElement( "property", new XAttribute( "name", string.Empty ) ) ) );
+            DudVisitor visitor = new DudVisitor();
+            DefinitionXmlValidator validator = new DefinitionXmlValidator( visitor );
+            validator.VisitAlgorithm( element );
+
+            Assert.IsFalse( visitor.DidVisitAlgorithm );
+        }
+
+        /// <summary>
+        /// Tests validating an algorithm with one property without the type
+        /// attribute
+        /// </summary>
+        [TestMethod]
+        public void TestValidateAlgorithm_OneProperty_NoTypeAttribute()
+        {
+            XElement element = new XElement(
+                "algorithm", new XAttribute( "name", "test" ),
+                new XElement( "properties",
+                    new XElement( "property",
+                        new XAttribute( "name", "test" ) ) ) );
+            DudVisitor visitor = new DudVisitor();
+            DefinitionXmlValidator validator = new DefinitionXmlValidator( visitor );
+            validator.VisitAlgorithm( element );
+
+            Assert.IsFalse( visitor.DidVisitAlgorithm );
+        }
+
+        /// <summary>
+        /// Tests validating an algorithm with a property containing an invalid type.
+        /// </summary>
+        [TestMethod]
+        public void TestValidateAlgorithm_OneProperty_InvalidTypeValue()
+        {
+            XElement element = new XElement(
+                "algorithm", new XAttribute( "name", "test" ),
+                new XElement( "properties",
+                    new XElement( "property",
+                        new XAttribute( "name", string.Empty ),
+                        new XAttribute( "type", "notatype" ) ) ) );
+            DudVisitor visitor = new DudVisitor();
+            DefinitionXmlValidator validator = new DefinitionXmlValidator( visitor );
+            validator.VisitAlgorithm( element );
+
+            Assert.IsFalse( visitor.DidVisitAlgorithm );
+        }
+
+        /// <summary>
+        /// Tests validating an algorithm with a property with no value
+        /// </summary>
+        [TestMethod]
+        public void TestValidateAlgorithm_OneProperty_NoValueAttr_NoValueElement()
+        {
+            XElement element = new XElement(
+                "algorithm", new XAttribute( "name", "test" ),
+                new XElement( "properties",
+                    new XElement( "property",
+                        new XAttribute( "name", string.Empty ),
+                        new XAttribute( "type", typeof( double ) ) ) ) );
+            DudVisitor visitor = new DudVisitor();
+            DefinitionXmlValidator validator = new DefinitionXmlValidator( visitor );
+            validator.VisitAlgorithm( element );
+
+            Assert.IsFalse( visitor.DidVisitAlgorithm );
+        }
+
+        /// <summary>
+        /// Tests validating an algorithm with a property contained within a
+        /// nested element
+        /// </summary>
+        [TestMethod]
+        public void TestValidateAlgorithm_OneProperty_NoValueAttr_ValueElement()
+        {
+            XElement element = new XElement(
+                "algorithm", new XAttribute( "name", "test" ),
+                new XElement( "properties",
+                    new XElement( "property",
+                        new XAttribute( "name", "test" ),
+                        new XAttribute( "type", typeof( double ) ),
+                        new XElement( "value" ) ) ) );
+            DudVisitor visitor = new DudVisitor();
+            DefinitionXmlValidator validator = new DefinitionXmlValidator( visitor );
+            validator.VisitAlgorithm( element );
+
+            Assert.IsTrue( visitor.DidVisitAlgorithm );
+        }
+
+        /// <summary>
+        /// Tests validating an algorithm with a property containing a value
+        /// as an attribute
+        /// </summary>
+        [TestMethod]
+        public void TestValidateAlgorithm_OneProperty_ValueAttr()
+        {
+            XElement element = new XElement(
+                "algorithm", new XAttribute( "name", "test" ),
+                new XElement( "properties",
+                    new XElement( "property",
+                        new XAttribute( "name", "test" ),
+                        new XAttribute( "type", typeof( double ) ),
+                        new XAttribute( "value", "1" ) ) ) );
+            DudVisitor visitor = new DudVisitor();
+            DefinitionXmlValidator validator = new DefinitionXmlValidator( visitor );
+            validator.VisitAlgorithm( element );
+
+            Assert.IsTrue( visitor.DidVisitAlgorithm );
+        }
+
+
+        class DudVisitor : IJobXmlVisitor
+        {
+            public bool DidVisitAlgorithm
+            {
+                get;
+                set;
+            }
+
+            public void VisitAlgorithm( XNode xml )
+            {
+                DidVisitAlgorithm = true;
+            }
+
+            public void VisitInput( XNode xml )
+            {
+            }
+        }
+    }
+}
---
+++ src/Processor/Client/XML/Decompilation/DefinitionXmlValidator.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DIPS.Processor.XML.Decompilation
+{
+    /// <summary>
+    /// Represents the Xml validator that ensures definition Xml is valid.
+    /// </summary>
+    public class DefinitionXmlValidator : ValidationVisitor
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DefinitionXmlValidator"/>
+        /// class.
+        /// </summary>
+        /// <param name="visitor">The <see cref="IJobXmlVisitor"/> this
+        /// <see cref="XmlVisitorDecorate"/> decorates.</param>
+        public DefinitionXmlValidator( IJobXmlVisitor visitor )
+            : base( visitor )
+        {
+        }
+
+
+        /// <summary>
+        /// Executes the algorithm-node validation logic.
+        /// </summary>
+        /// <param name="algorithmNode">The <see cref="XNode"/> representing an
+        /// algorithm.</param>
+        /// <returns><c>true</c> if the algorithm represented by the
+        /// <see cref="XNode"/> is valid; false otherwise.</returns>
+        protected override bool IsAlgorithmValid( XNode algorithmNode )
+        {
+            if( algorithmNode.NodeType != System.Xml.XmlNodeType.Element )
+            {
+                return false;
+            }
+
+            XElement element = (XElement)algorithmNode;
+            if( element.Name != "algorithm" )
+            {
+                return false;
+            }
+
+            XAttribute nameAttr = element.Attribute( "name" );
+            if( nameAttr == null )
+            {
+                return false;
+            }
+
+            if( string.IsNullOrEmpty( nameAttr.Value ) )
+            {
+                return false;
+            }
+
+            // Algorithms may not have properties!
+            var properties = element.Descendants( "properties" );
+            if( properties.Any() )
+            {
+                return _validateProperties( properties.First() );
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Executes the input-node validation logic.
+        /// </summary>
+        /// <param name="algorithmNode">The <see cref="XNode"/> representing an
+        /// input.</param>
+        /// <returns><c>true</c> if the input represented by the
+        /// <see cref="XNode"/> is valid; false otherwise.</returns>
+        protected override bool IsInputValid( XNode inputNode )
+        {
+            throw new InvalidOperationException( "Job definitions do not support inputs." );
+        }
+
+
+        /// <summary>
+        /// Validates the properties of an algorithm.
+        /// </summary>
+        /// <param name="properties">The properties to validate.</param>
+        /// <returns>true if all properties are valid.</returns>
+        private bool _validateProperties( XElement properties )
+        {
+            var subProperties = properties.Descendants( "property" );
+            foreach( var property in subProperties )
+            {
+                if( _validateProperty( property ) == false )
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Validates a single property of an algorithm.
+        /// </summary>
+        /// <param name="property">The property to validate</param>
+        /// <returns>true if the property is valid</returns>
+        private bool _validateProperty( XElement property )
+        {
+            XAttribute nameAttr = property.Attribute( "name" );
+            if( nameAttr == null )
+            {
+                return false;
+            }
+
+            if( string.IsNullOrEmpty( nameAttr.Value ) )
+            {
+                return false;
+            }
+
+            XAttribute typeAttr = property.Attribute( "type" );
+            if( typeAttr == null )
+            {
+                return false;
+            }
+
+            string typeName = typeAttr.Value;
+            try
+            {
+                Type.GetType( typeName );
+            }
+            catch
+            {
+                return false;
+            }
+
+            XAttribute valueAttr = property.Attribute( "value" );
+            if( valueAttr == null )
+            {
+                // May be using nested element.
+                var valueElements = property.Descendants( "value" );
+                if( valueElements.Any() == false )
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
