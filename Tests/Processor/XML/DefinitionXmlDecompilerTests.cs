8c1f7d610bd42830ad6685ec4268baeba62e8c2d
Sh3z <t.sherwood@me.com>
2014-02-07 16:39:17 +0000

    Added DefinitionXmlDecompiler

---
+++ Tests/Processor/XML/DefinitionXmlDecompilerTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DIPS.Processor.Client;
+using DIPS.Processor.XML.Decompilation;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace DIPS.Tests.Processor.XML
+{
+    /// <summary>
+    /// Summary description for DefinitionXmlDecompilerTests
+    /// </summary>
+    [TestClass]
+    public class DefinitionXmlDecompilerTests
+    {
+        /// <summary>
+        /// Gets or sets the test context which provides
+        /// information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Tests decompiling an algorithm from a node that is not an XElement
+        /// </summary>
+        [TestMethod]
+        [ExpectedException( typeof( ArgumentException ) )]
+        public void TestDecompileAlgorithm_NotElement()
+        {
+            XText t = new XText( "Test" );
+            DefinitionXmlDecompiler d = new DefinitionXmlDecompiler();
+            d.DecompileAlgorithm( t );
+        }
+
+        /// <summary>
+        /// Tests decompiling an algorithm with no properties.
+        /// </summary>
+        [TestMethod]
+        public void TestDecompileAlgorithm_NoProperties()
+        {
+            DefinitionXmlDecompiler decompiler = new DefinitionXmlDecompiler();
+            XElement xml = new XElement( "algorithm",
+                new XAttribute( "name", "gamma" ) );
+            AlgorithmDefinition a = decompiler.DecompileAlgorithm( xml );
+
+            Assert.AreEqual( "gamma", a.AlgorithmName );
+            Assert.AreEqual( 0, a.Properties.Count );
+        }
+
+        /// <summary>
+        /// Tests decompiling an algorithm with one property of a primitive type.
+        /// </summary>
+        [TestMethod]
+        public void TestDecompileAlgorithm_OneProperty_Primitive()
+        {
+            DefinitionXmlDecompiler decompiler = new DefinitionXmlDecompiler();
+            XElement xml = new XElement( "algorithm",
+                new XAttribute( "name", "gamma" ),
+                new XElement( "properties",
+                    new XElement( "property",
+                        new XAttribute( "name", "gamma" ),
+                        new XAttribute( "type", typeof( double ) ),
+                        new XAttribute( "value", "1" ) ) ) );
+            AlgorithmDefinition a = decompiler.DecompileAlgorithm( xml );
+
+            Assert.AreEqual( 1, a.Properties.Count );
+
+            Property gamma = a.Properties.First();
+            Assert.AreEqual( "gamma", gamma.Name );
+            Assert.AreEqual( typeof( double ), gamma.Type );
+            Assert.IsNull( gamma.Converter );
+            Assert.IsNull( gamma.Compressor );
+            Assert.AreEqual( 1d, gamma.Value );
+        }
+    }
+}
---
+++ src/Processor/Client/XML/Decompilation/DefinitionXmlDecompiler.cs
@@ -0,0 +1,94 @@
+﻿using DIPS.Processor.Client;
+using DIPS.Processor.Client.JobDeployment;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace DIPS.Processor.XML.Decompilation
+{
+    /// <summary>
+    /// Provides decompilation behaviour for definition Xml.
+    /// </summary>
+    public class DefinitionXmlDecompiler : IAlgorithmXmlDecompiler
+    {
+        /// <summary>
+        /// Decompiles an Xml node back into an <see cref="AlgorithmDefinition"/>.
+        /// </summary>
+        /// <param name="algorithmNode">The <see cref="XNode"/> representing the
+        /// <see cref="AlgorithmDefinition"/>.</param>
+        /// <returns>An <see cref="AlgorithmDefinition"/> object represented by
+        /// the provided Xml.</returns>
+        public AlgorithmDefinition DecompileAlgorithm( XNode algorithmNode )
+        {
+            if( algorithmNode.NodeType != XmlNodeType.Element )
+            {
+                throw new ArgumentException( "An XElement is required." );
+            }
+
+            XElement element = (XElement)algorithmNode;
+            IEnumerable<Property> properties = new List<Property>();
+            var propertiesNodes = element.Descendants( "properties" );
+            if( propertiesNodes.Any() )
+            {
+                properties = _parseProperties( propertiesNodes.First() );
+            }
+
+            string name = element.Attribute( "name" ).Value;
+            return new AlgorithmDefinition( name, properties );
+        }
+
+        /// <summary>
+        /// Decompiles an Xml node back into a <see cref="JobInput"/>.
+        /// </summary>
+        /// <param name="inputNode">The <see cref="XNode"/> represnting the
+        /// <see cref="JobInput."/></param>
+        /// <returns>A <see cref="JobInput"/> object represented by the provided
+        /// Xml.</returns>
+        public JobInput DecompileInput( XNode inputNode )
+        {
+            throw new InvalidOperationException( "Definition Xml does not support images." );
+        }
+
+
+        /// <summary>
+        /// Parses the properties node.
+        /// </summary>
+        /// <param name="properties">The "properties" node from Xml</param>
+        /// <returns>The set of Property objects represented by the Xml.</returns>
+        private IEnumerable<Property> _parseProperties( XElement properties )
+        {
+            ICollection<Property> props = new List<Property>();
+            foreach( var propertyNode in properties.Descendants( "property" ) )
+            {
+                Property property = _parseProperty( propertyNode );
+                props.Add( property );
+            }
+
+            return props;
+        }
+
+        /// <summary>
+        /// Parses a single property from Xml
+        /// </summary>
+        /// <param name="property">The proeprty element</param>
+        /// <returns>A Property instance representing the Xml.</returns>
+        private Property _parseProperty( XElement property )
+        {
+            string typeAsString = property.Attribute( "type" ).Value;
+            Type propertyType = Type.GetType( typeAsString );
+            TypeConverter valConverter = TypeDescriptor.GetConverter( propertyType );
+            string valueAsString = property.Attribute( "value" ).Value;
+
+            PropertyBuilder builder = new PropertyBuilder();
+            builder.Name = property.Attribute( "name" ).Value;
+            builder.PropertyType = propertyType;
+            builder.DefaultValue = valConverter.ConvertFromString( valueAsString );
+            return builder.Build();
+        }
+    }
+}
