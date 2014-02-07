84fc6650fb16475fd5b9a365aaa39705fdda0fec
Sh3z <t.sherwood@me.com>
2014-02-07 12:31:07 +0000

    Exposed algorithm definitions as objects

--- src/Processor/ProcessingService.cs
+++ src/Processor/ProcessingService.cs
@@ -53,6 +53,17 @@ namespace DIPS.Processor
         private XDocument _algorithms;
 
         /// <summary>
+        /// Returns an enumerable set of <see cref="AlgorithmDefinition"/>s in
+        /// object form.
+        /// </summary>
+        /// <returns>The set of <see cref="AlgorithmDefinition"/>s this service can
+        /// understand.</returns>
+        public IEnumerable<AlgorithmDefinition> GetAlgorithmDefinitions()
+        {
+            return RegistryCache.Cache.KnownAlgorithms;
+        }
+
+        /// <summary>
         /// Creates and returns a new <see cref="ISynchronousProcessor"/> clients
         /// can use to compute jobs on their own threads.
         /// </summary>
