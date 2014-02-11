ce2f44b80ca4cf01a1dd956437b0c1280a5cceaa
Sh3z <t.sherwood@me.com>
2014-02-11 16:23:44 +0000

    Added TemporaryFile

---
+++ Tests/Processor/Plugin/Matlab/TemporaryFileTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DIPS.Processor.Plugin.Matlab;
+using System.IO;
+
+namespace DIPS.Tests.Processor.Plugin.Matlab
+{
+    /// <summary>
+    /// Summary description for TemporaryFileTests
+    /// </summary>
+    [TestClass]
+    public class TemporaryFileTests
+    {
+        /// <summary>
+        /// Gets or sets the test context which provides
+        /// information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Tests creating a temporary file with no name.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException( typeof( ArgumentException ) )]
+        public void TestConstructor_NullName()
+        {
+            byte[] bytes = new byte[0];
+            TemporaryFile f = new TemporaryFile( null, bytes );
+        }
+
+        /// <summary>
+        /// Tests creating a temporary file with an empty name
+        /// </summary>
+        [TestMethod]
+        [ExpectedException( typeof( ArgumentException ) )]
+        public void TestConstructor_EmptyName()
+        {
+            byte[] bytes = new byte[0];
+            TemporaryFile f = new TemporaryFile( string.Empty, bytes );
+        }
+
+        /// <summary>
+        /// Tests creating a temporary file with null bytes
+        /// </summary>
+        [TestMethod]
+        [ExpectedException( typeof( ArgumentNullException ) )]
+        public void TestConstructor_NullBytes()
+        {
+            string name = "Test.txt";
+            TemporaryFile f = new TemporaryFile( name, null );
+        }
+
+        /// <summary>
+        /// Tests creating a valid temporary file. This test will release the
+        /// file.
+        /// </summary>
+        [TestMethod]
+        public void TestConstructor_Valid_NonAutoRelease()
+        {
+            string name = "Test.txt";
+            byte[] bytes = File.ReadAllBytes( "TestFile.txt" );
+            TemporaryFile f = new TemporaryFile( name, bytes );
+
+            Assert.IsTrue( File.Exists( f.FilePath ) );
+
+            File.Delete( f.FilePath );
+        }
+
+        /// <summary>
+        /// Tests creating a valid temporary file and auto-releasing the file.
+        /// </summary>
+        [TestMethod]
+        public void TestConstructor_Valid_AutoRelease()
+        {
+            string name = "Test.txt";
+            byte[] bytes = File.ReadAllBytes( "TestFile.txt" );
+            string path = string.Empty;
+            using( TemporaryFile f = new TemporaryFile( name, bytes ) )
+            {
+                path = f.FilePath;
+                Assert.IsTrue( File.Exists( path ) );
+            }
+
+            Assert.IsFalse( File.Exists( path ) );
+        }
+    }
+}
---
+++ src/Processor/Plugin/Matlab/TemporaryFile.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPS.Processor.Plugin.Matlab
+{
+    /// <summary>
+    /// Represents a temporary file on-disk for allowing other functions to
+    /// access bytes as a file.
+    /// </summary>
+    public class TemporaryFile : IDisposable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TemporaryFile"/> class
+        /// that will output a file into the current directory.
+        /// </summary>
+        /// <param name="fileName">The name (with extension) to give to the new
+        /// file.</param>
+        /// <param name="fileContents">The raw contents to provide to the
+        /// file.</param>
+        /// <exception cref="ArgumentException">fileName is null or empty.</exception>
+        /// <exception cref="ArgumentNullException">fileContents is null.</exception>
+        /// <exception cref="IOException">file cannot be created within the current
+        /// directory.</exception>
+        public TemporaryFile( string fileName, byte[] fileContents )
+        {
+            if( string.IsNullOrEmpty( fileName ) )
+            {
+                throw new ArgumentException( "fileName" );
+            }
+
+            if( fileContents == null )
+            {
+                throw new ArgumentNullException( "fileContents" );
+            }
+
+            FilePath = string.Format( @"{0}/{1}", Directory.GetCurrentDirectory(), fileName );
+            _saveFile( fileContents );
+        }
+
+
+        /// <summary>
+        /// Gets the temporary path given to the file.
+        /// </summary>
+        public string FilePath
+        {
+            get;
+            private set;
+        }
+
+
+        /// <summary>
+        /// Disposes of the resources held by this <see cref="TemporaryFile"/> -
+        /// i.e. deletes the file.
+        /// </summary>
+        public void Dispose()
+        {
+            if( File.Exists( FilePath ) )
+            {
+                File.Delete( FilePath );
+            }
+        }
+
+
+        /// <summary>
+        /// Saves the contents of the file to the current path.
+        /// </summary>
+        /// <param name="file">The bytes to save to the file.</param>
+        private void _saveFile( byte[] file )
+        {
+            try
+            {
+                File.WriteAllBytes( FilePath, file );
+            }
+            catch( Exception e )
+            {
+                throw new IOException( "Unable to save file. See inner exception.", e );
+            }
+        }
+    }
+}
