90daf22e9ee519cdad002b03609e0ae43513a081
GaryClaret <Gary.Chadwick1@btinternet.com>
2014-02-14 12:46:46 +0000

    Adding of View Models in folder and registration in App file

---
+++ src/ViewModel/UserInterfaceVM/MainNaviViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using DIPS.Processor.Client;
+
+namespace DIPS.ViewModel.UserInterfaceVM
+{
+    public class MainNaviViewModel : Window
+    {
+        public MainNaviViewModel()
+        {
+            //InitializeComponent();
+            _main = new MainViewModel();
+            //contentFrame.Navigate( _main );
+        }
+
+
+        /// <summary>
+        /// Gets or sets the <see cref="IProcessingService"/> connected to
+        /// the active DIPS processor.
+        /// </summary>
+        
+        public IProcessingService Service
+        {
+            get
+            {
+                return _main.Service;
+            }
+            set
+            {
+                _main.Service = value;
+            }
+        }
+
+
+
+        /// <summary>
+        /// Contains a reference to the root page view.
+        /// </summary>
+        private MainViewModel _main;
+    }
+}
---
+++ src/ViewModel/UserInterfaceVM/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DIPS.Processor.Client;
+
+namespace DIPS.ViewModel.UserInterfaceVM
+{
+    public class MainViewModel
+    {
+        public ICommand ViewExistingDataSetCommand { get; private set; }
+    
+        public IProcessingService Service { get; set; }
+    }
+
+    }
