ab5d7fac477c1151aa003ef1310df113b5b0fc50
Sh3z <t.sherwood@me.com>
2014-02-17 14:36:37 +0000

    BaseViewModel now extends Viewmodel

--- src/ViewModel/BaseViewModel.cs
+++ src/ViewModel/BaseViewModel.cs
@@ -9,13 +9,11 @@ using DIPS.ViewModel.UserInterfaceVM;
 
 namespace DIPS.ViewModel
 {
-    public abstract class BaseViewModel : INotifyPropertyChanged
+    public abstract class BaseViewModel : ViewModel
     {
         public static BaseViewModel ViewModel { get; set; }
         
         public static Frame OverallFrame { get; set; }
-
-        public event PropertyChangedEventHandler PropertyChanged;
 
         readonly public static ViewExistingDatasetViewModel _ViewExistingDatasetViewModel = new ViewExistingDatasetViewModel();
         readonly public static LoadNewDsStep1ViewModel _LoadNewDsStep1ViewModel = new LoadNewDsStep1ViewModel();
